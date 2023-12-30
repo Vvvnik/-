81976cdd5db1a02ff10ca49f0513c974b12c6dfa
--add <gorbachev@anc.space>
2023-12-30 17:14:18 +0300

    123

--- 123.ps1
+++ 123.ps1
@@ -4,7 +4,7 @@ $workNumber = "номер-работы"
 $author = "фамилия-автора"
 
 $command = "asciidoctor-pdf"
-$arguments = "-T "-r custom/10titlePage.rb", "-r custom/20appendixCustomCaption.rb", "-r custom/30customFigureCaption.rb", "-a pdf-fontsdir=resources/fonts", "-a pdf-themesdir=resources/themes", "report.adoc", "--theme=report", "-o '$group-$subject-$workNumber-$author.pdf'"
+$arguments = "-T ","-r custom/10titlePage.rb","-r custom/20appendixCustomCaption.rb","-r custom/30customFigureCaption.rb","-a pdf-fontsdir=resources/fonts", "-a pdf-themesdir=resources/themes", "report.adoc", "--theme=report", "-o '$group-$subject-$workNumber-$author.pdf'"
 
 try {
     $process = Start-Process -FilePath $command -ArgumentList $arguments -NoNewWindow -Wait -PassThru
