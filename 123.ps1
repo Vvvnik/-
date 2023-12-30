7765aa7d1482917b3795b5b5299e87a9b60678f3
--add <gorbachev@anc.space>
2023-12-30 16:48:43 +0300

    wefewfw

---
+++ 123.ps1
@@ -0,0 +1,20 @@
+$group = "номер-группы"
+$subject = "название-предмета"
+$workNumber = "номер-работы"
+$author = "фамилия-автора"
+
+$command = "asciidoctor-pdf"
+$arguments = "-T "-r custom/10titlePage.rb", "-r custom/20appendixCustomCaption.rb", "-r custom/30customFigureCaption.rb", "-a pdf-fontsdir=resources/fonts", "-a pdf-themesdir=resources/themes", "report.adoc", "--theme=report", "-o '$group-$subject-$workNumber-$author.pdf'"
+
+try {
+    $process = Start-Process -FilePath $command -ArgumentList $arguments -NoNewWindow -Wait -PassThru
+    if ($process.ExitCode -eq 0) {
+        Write-Host "Report generated!"
+    }
+    else {
+        throw "Error generating report. Exit code: $($process.ExitCode)"
+    }
+}
+catch {
+    Write-Error $_.Exception.Message
+}
